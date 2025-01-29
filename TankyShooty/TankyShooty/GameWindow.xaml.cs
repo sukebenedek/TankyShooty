e8223f989d6027fcd067cac8a7e25a473c8304fd
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2025-01-29 11:29:42 +0100

    rectangles

---
+++ TankyShooty/TankyShooty/GameWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TankyShooty
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        public GameWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+
+            MyCanvas.Focus();
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
