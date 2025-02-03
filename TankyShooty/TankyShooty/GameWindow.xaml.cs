cc6ca5c3ff04199560f37dac43356e0a6de99e95
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2025-02-03 10:30:40 +0100

    mozgás, lövés elkezdése #4 #5

--- TankyShooty/TankyShooty/GameWindow.xaml.cs
+++ TankyShooty/TankyShooty/GameWindow.xaml.cs
@@ -21,6 +21,13 @@ namespace TankyShooty
     public partial class GameWindow : Window
     {
         DispatcherTimer gameTimer = new DispatcherTimer();
+        bool moveForwardPlayer1, moveBackwardPlayer1, moveForwardPlayer2, moveBackwardPlayer12 = false;
+        int playerSpeed = 10;
+        List<Rectangle> itemRemover = new List<Rectangle>();
+        List<int> scores = new List<int>(2);
+
+        Rect player1Hitbox;
+        Rect player2Hitbox;
         public GameWindow()
         {
             InitializeComponent();
@@ -30,21 +37,68 @@ namespace TankyShooty
             gameTimer.Start();
 
             MyCanvas.Focus();
+
+            //ImageBrush player1Image = new ImageBrush();
+            //player1Image.ImageSource = new BitmapImage(new Uri("url here"));
+            //Player1.Fill = playerImgae();
         }
 
         private void GameLoop(object sender, EventArgs e)
         {
+            player1Hitbox = new Rect(Canvas.GetLeft(Player1), Canvas.GetTop(Player1), Player1.Width, Player1.Height);
 
+            //scoreText.Content = $"{scores[0]} - {scores[1]}";
+
+            if (moveForwardPlayer1 == true)
+            {
+                Canvas.SetTop(Player1, Canvas.GetTop(Player1) - playerSpeed);
         }
+            if(moveBackwardPlayer1 == true)
+            {
+                Canvas.SetTop(Player1, Canvas.GetTop(Player1) + playerSpeed);
+            }
+}
 
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Up) 
+            {
+                moveForwardPlayer1 = true;
+            }
+            if (e.Key == Key.Down) 
+            {
+                moveBackwardPlayer1 = true;
+            }
 
         }
 
         private void OnKeyUp(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Up)
+            {
+                moveForwardPlayer1 = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                moveBackwardPlayer1 = false;
+            }
 
+            if(e.Key == Key.Space) 
+            {
+                Rectangle newBulletPlayer1 = new Rectangle
+                {
+                    Tag = "bulletPlayer1",
+                    Height = 20,
+                    Width = 5,
+                    Fill = Brushes.White,
+                    Stroke = Brushes.Red,
+                };
+
+                Canvas.SetLeft(newBulletPlayer1, Canvas.GetLeft(Player1) + Player1.Width / 2);
+                Canvas.SetTop(newBulletPlayer1, Canvas.GetTop(Player1) - newBulletPlayer1.Height);
+
+                MyCanvas.Children.Add(newBulletPlayer1);
+            }
         }
     }
 }
